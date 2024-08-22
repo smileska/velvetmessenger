f566c62908289bf533333822c4ee4b2b0f8ee27e
Eva Smileska <esmileska7@gmail.com>
2024-08-22 12:13:35 +0200

    Added a notification system for private messages and added more fixes to the chats and chatrooms

--- src/Views/parts/navbar.php
+++ src/Views/parts/navbar.php
@@ -13,15 +13,17 @@
                     <img id="sunIcon" src="https://img.icons8.com/ios/50/FFFFFF/sun--v1.png" alt="Sun Icon" class="hidden" />
                 </button>
                 <?php if(isset($_SESSION['username'])) : ?>
-                    <div id="notificationBell" class="relative cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                        </svg>
-                        <span id="notificationCount" class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full hidden">0</span>
-                    </div>
-                    <div id="notificationDropdown" class="hidden absolute right-0 mt-2 w-80 bg-white rounded-md shadow-lg overflow-hidden z-20">
-                        <div class="py-2" id="notificationList">
-                            <div class="px-4 py-2 text-sm text-gray-700">No new notifications</div>
+                    <div class="relative">
+                        <button id="notificationBell" class="relative text-white focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                            </svg>
+                            <span id="notificationCount" class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full hidden">0</span>
+                        </button>
+                        <div id="notificationDropdown" class="absolute right-0 mt-2 w-80 bg-white rounded-md shadow-lg overflow-hidden z-20 hidden">
+                            <div class="py-2" id="notificationList">
+                                <div class="px-4 py-2 text-sm text-gray-700 no-notifications">No new notifications</div>
+                            </div>
                         </div>
                     </div>
                     <a href="/profile">
